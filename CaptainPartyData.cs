997e0f05cd7793e225200e1df0f2cff7a711d06b
Fox <onefox@gmail.com>
2023-11-25 22:20:51 -0800

    Initial Commit
    Arf

---
+++ CaptainPartyData.cs
@@ -0,0 +1,39 @@
+﻿using Mafi;
+using Mafi.Base;
+using Mafi.Core.Entities.Static.Layout;
+using Mafi.Core.Mods;
+
+namespace CaptainParty
+{
+    internal class CaptainPartyData : IModData
+    {
+        public void RegisterData(ProtoRegistrator registrator)
+        {
+            registrator.StorageProtoBuilder
+                .Start("Party Test", CaptainPartyModIds.Machines.PartyPlace)
+                .Description("Something to test")
+                .SetCapacity(1024)                
+                .SetNoTransferLimit()
+                .SetCost(Costs.Build.CP(80).Workers(1))
+                .SetLayout(new EntityLayoutParams(),
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]",
+                    "[4][4][4][4][4][4][4][4][4][4][4][4][4]")
+                .SetPrefabPath("Assets/Base/Buildings/Ruins/RadioTower.prefab")
+                .SetPrefabOffset(-RelTile3f.UnitX)
+                .SetCategories(Ids.ToolbarCategories.Buildings)
+                .BuildAndAdd();
+        }
+    }
+}
---
+++ CaptainPartyMod.cs
@@ -0,0 +1,30 @@
+﻿using Mafi.Base;
+using Mafi.Core;
+using Mafi;
+using Mafi.Core.Mods;
+
+namespace CaptainParty
+{
+    public sealed class CaptainPartyMod : DataOnlyMod
+    {
+        public override string Name => "Captain Party";
+
+        public override int Version => 1;
+
+        public CaptainPartyMod(CoreMod coreMod, BaseMod baseMod)
+        {
+            // You can use Log class for logging. These will be written to the log file
+            // and can be also displayed in the in-game console with command `also_log_to_console`.
+            Log.Info("CaptainPartyMod: constructed");
+        }
+
+        public override void RegisterPrototypes(ProtoRegistrator registrator)
+        {
+            Log.Info("CaptainPartyMod: registering prototypes");
+
+            registrator.RegisterAllProducts();
+
+            registrator.RegisterData<CaptainPartyData>();
+        }
+    }
+}
